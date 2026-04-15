498f3a6a182e94a6a2204d2542c6495b2cc04d94
Sam Kohn <samuelikohn@gmail.com>
2026-04-14 21:16:42 -0500

    Fix: confirm check/reveal overlays close correctly

--- src/components/ConfirmCheck.jsx
+++ src/components/ConfirmCheck.jsx
@@ -2,14 +2,19 @@ import ModalLayer from "./ModalLayer.jsx"
 import "../styles/Overlay.css"
 
 export default function ConfirmCheck(props) {
+    function handleConfirm() {
+        props.checkAllBoxes()
+        props.onClose()
+    }
+
     return (
         <ModalLayer contentClassName="overlay confirmDiv" onClose={props.onClose}>
             {({ titleId }) => (
                 <>
                     <h2 id={titleId}>{`Are you sure you want to check the entire puzzle?`}</h2>
                     <div className="confirmBtns">
-                        <button onClick={props.checkAllBoxes}>Yes</button>
-                        <button onClick={props.onClose}>No</button>
+                        <button onClick={handleConfirm} type="button">Yes</button>
+                        <button onClick={props.onClose} type="button">No</button>
                     </div>
                 </>
             )}
--- src/components/ConfirmReveal.jsx
+++ src/components/ConfirmReveal.jsx
@@ -2,14 +2,19 @@ import ModalLayer from "./ModalLayer.jsx"
 import "../styles/Overlay.css"
 
 export default function ConfirmReveal(props) {
+    function handleConfirm() {
+        props.revealAllBoxes()
+        props.onClose()
+    }
+
     return (
         <ModalLayer contentClassName="overlay confirmDiv" onClose={props.onClose}>
             {({ titleId }) => (
                 <>
                     <h2 id={titleId}>{`Are you sure you want to reveal the entire puzzle?`}</h2>
                     <div className="confirmBtns">
-                        <button onClick={props.revealAllBoxes}>Yes</button>
-                        <button onClick={props.onClose}>No</button>
+                        <button onClick={handleConfirm} type="button">Yes</button>
+                        <button onClick={props.onClose} type="button">No</button>
                     </div>
                 </>
             )}
